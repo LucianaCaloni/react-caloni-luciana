29b70f1ddcaa674b714b498859b63fc5da9985e4
Luciana Caloni <luciana.caloni@gmail.com>
2022-06-12 21:58:08 +0900

    contador desafio

---
+++ src/components/ItemListContainer/ItemCount/ItemCount.js
@@ -0,0 +1,31 @@
+import "./ItemCount.css";
+import { useState } from "react";
+
+function ItemCount({ stock, initial }) {
+  const [cant, setCount] = useState(initial);
+  function suma() {
+    if (cant < stock) {
+      setCount(cant + 1);
+    } else {
+      alert("Llegaste al limite maximo de productos")
+    }
+  }
+
+  function resta() {
+    if (cant > initial) {
+      setCount(cant - 1);
+    }
+  }
+
+  return (
+    <div className="contador" >
+      <button  className= "contador-num"onClick={resta}>-</button>
+      <span className= "contador-num">{cant}</span>
+      <button className= "contador-num" onClick={suma}>+</button>
+      <br />
+      <button className= "contador-button">Add to cart</button>
+    </div>
+  );
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,12 @@
+
 import "./ItemListContainer.css";
+import ItemCount from "./ItemCount/ItemCount";
+
 export default function ItemListContainer(props) {
   return (
     <div className="container">
       <p>{props.gretting}</p>
+      <ItemCount stock={5} initial={1}/>
     </div>
   );
 }
